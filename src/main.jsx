6514a93887c4791754d77dea107ef45fc931072c
Victor Gardner <victorgardner@mac.lan>
2025-07-17 19:03:58 -0500

    Fix: case-sensitive imports for Netlify

--- src/main.jsx
+++ src/main.jsx
@@ -2,8 +2,8 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter, Routes, Route, Link } from 'react-router-dom'
 import Home from './Home'
-import ClearChoice from './ClearChoice'
-import SwitchCommerce from './SwitchCommerce'
+import ClearChoice from './ClearChoice.jsx'
+import SwitchCommerce from './SwitchCommerce.jsx'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
